4cbd06811a2e6d7e28d4745dbcad4b7ae217c1c2
Stanislav Kravchenko <krstaniskav2018@gmail.com>
2024-09-07 23:29:36 +0300

    Updates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateMonthDto } from './dto/create-month.dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller()
 export class AppController {
@@ -19,5 +20,21 @@ export class AppController {
   @Get('current')
   getCurrentMonth() {
     return this.appService.getCurrentMonth();
+  }
+
+  @Post('add/:year/:month')
+  addExpensesInMonth(
+    @Param('year') year: number,
+    @Param('month') month: string,
+    @Body() category: CreateCategoryDto,
+  ) {
+    year = parseInt(String(year));
+    return this.appService.addExpensesInMonth(year, month, category);
+  }
+
+  @Get(':year/:month')
+  getMonth(@Param('year') year: number, @Param('month') month: string) {
+    year = parseInt(String(year));
+    return this.appService.getMonth(year, month);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,27 +2,35 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateMonthDto } from './dto/create-month.dto';
 import { Prisma } from '@prisma/client';
-import { months } from "./utils/months";
+import { months } from './utils/months';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { DefaultArgs } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
-  async createMonth(createMonthDto: CreateMonthDto) {
-    const { name, year, income, categories } = createMonthDto;
-    const isUsed = await this.prisma.month.findFirst({
-      where: {
-        name,
-        year,
+  async createMonth(createMonthDto: CreateMonthDto): Promise<
+    Prisma.Prisma__MonthClient<
+      {
+        id: number;
+        name: string;
+        year: number;
+        income: number;
       },
-    });
+      never,
+      DefaultArgs
+    >
+  > {
+    const { name, year, income, categories } = createMonthDto;
+    const isUsed = await this.findMonthByNameAndYear(year, name);
 
     if (isUsed) {
       throw new HttpException(
         `Month ${name} in ${year} year is already exists.`,
         409,
       );
-    };
+    }
 
     const data: Prisma.MonthCreateInput = {
       name,
@@ -38,7 +46,11 @@ export class AppService {
 
     return this.prisma.month.create({ data });
   }
-  getMonthsInYear(year: number) {
+  getMonthsInYear(
+    year: number,
+  ): Prisma.PrismaPromise<
+    { id: number; name: string; year: number; income: number }[]
+  > {
     return this.prisma.month.findMany({
       where: {
         year,
@@ -46,15 +58,106 @@ export class AppService {
     });
   }
 
-  getCurrentMonth() {
+  getCurrentMonth(): Prisma.Prisma__MonthClient<
+    {
+      categories: {
+        id: number;
+        name: string;
+        expenses: number[];
+        monthId: number;
+      }[];
+    } & { id: number; name: string; year: number; income: number },
+    null,
+    DefaultArgs
+  > {
     const date = new Date();
     const currentYear = date.getFullYear();
     const currentMonth = months[date.getMonth()];
-    return this.prisma.month.findFirst({
-      where: {
-        year: currentYear,
-        name: currentMonth,
+    return this.findMonthByNameAndYear(currentYear, currentMonth);
+  }
+
+  async addExpensesInMonth(
+    year: number,
+    month: string,
+    category: CreateCategoryDto,
+  ): Promise<string> {
+    const monthToAddExpenses = await this.findMonthByNameAndYear(year, month);
+    if (!monthToAddExpenses) {
+      throw new HttpException('Such month does not exist', 404);
+    }
+    const existingCategory = monthToAddExpenses.categories.find(
+      (cat) => category.name == cat.name,
+    );
+    if (!existingCategory) {
+      throw new HttpException(
+        'Such category in this month does not exist',
+        404,
+      );
+    }
+    await this.prisma.category.update({
+      where: { id: existingCategory.id },
+      data: {
+        expenses: {
+          push: category.expenses,
+        },
       },
     });
+    return 'Expenses added successfully.';
+  }
+
+  findMonthByNameAndYear(
+    year: number,
+    month: string,
+  ): Prisma.Prisma__MonthClient<
+    {
+      categories: {
+        id: number;
+        name: string;
+        expenses: number[];
+        monthId: number;
+      }[];
+    } & { id: number; name: string; year: number; income: number },
+    null,
+    DefaultArgs
+  > {
+    return this.prisma.month.findFirst({
+      where: {
+        name: month,
+        year,
+      },
+      include: {
+        categories: true,
+      },
+    });
+  }
+
+  getMonth(
+    year: number,
+    month: string,
+  ): Prisma.Prisma__MonthClient<
+    {
+      categories: {
+        id: number;
+        name: string;
+        expenses: number[];
+        monthId: number;
+      }[];
+    } & { id: number; name: string; year: number; income: number },
+    null,
+    DefaultArgs
+  > {
+    const newMonth = this.prisma.month.findFirst({
+      where: {
+        year,
+        name: month,
+      },
+      include: {
+        categories: true,
+      },
+    });
+    if (!newMonth) {
+      throw new HttpException('Such month does not exist', 404);
+    }
+    return newMonth;
   }
 }
